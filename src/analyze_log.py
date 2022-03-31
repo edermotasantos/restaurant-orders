2183e930fc09d8069a741d6806f6fa2a5e5b2a7a
Eder Santos <3derms@gmail.com>
2022-03-31 18:23:41 -0300

    feat: escreve respostas as perguntas

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,53 @@
+import csv
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    count = {}
+    count_hamburguer = {}
+    order_list = []
+    all_orders = set()
+    joao_orders = set()
+    days_of_the_week = set()
+    joao_went = set()
+
+    with open(path_to_file) as file:
+        orders_reader = csv.DictReader(file, fieldnames=["customer", "order", "day"])
+        for row in orders_reader:
+            order_list.append((row["customer"], row["order"], row["day"]))
+
+    for order in order_list:
+        most_frequent_order = order[1]
+        if order[0] == "maria":
+            if order not in count:
+                count[order[1]] = 1
+            else:
+                count[order[1]] += 1
+            if count[order[1]] > count[most_frequent_order]:
+                most_frequent_order = order[1]
+
+        if order[0] == "arnaldo" and order[1] == "hamburguer":
+            if "hamburguer" not in count_hamburguer:
+                count_hamburguer["hamburguer"] = 1
+            else:
+                count_hamburguer["hamburguer"] += 1
+
+        all_orders.add(order[1])
+        days_of_the_week.add(order[2])
+
+        if order[0] == "joao":
+            joao_orders.add(order[1])
+            joao_went.add(order[2])
+
+        for joao_order in joao_orders:
+            all_orders.discard(joao_order)
+
+        for day in joao_went:
+            days_of_the_week.discard(day)
+
+    with open("data/mkt_campaign.txt", "w") as txt_file:
+        txt_file.write(
+            most_frequent_order + "\n"
+            + str(count_hamburguer["hamburguer"]) + "\n"
+            + str(all_orders) + "\n" + str(days_of_the_week)
+        )
+    return None
