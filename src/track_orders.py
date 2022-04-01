0d80df26177c505ed746a4ceb9b3ebcc9fcf6341
Eder Santos <3derms@gmail.com>
2022-04-01 17:56:56 -0300

    feat: cria metodo que retorna o prato mais pedido

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,3 +1,32 @@
+import csv
+
+
+def most_ordered_dish(order_list, customer):
+    count = {}
+    for order in order_list:
+        most_frequent_order = order[1]
+        if order[0] == customer:
+            if order not in count:
+                count[order[1]] = 1
+            else:
+                count[order[1]] += 1
+            if count[order[1]] > count[most_frequent_order]:
+                most_frequent_order = order[1]
+    return most_frequent_order
+
+
+def analyze_log(path_to_file):
+    order_list = []
+        
+    with open(path_to_file) as file:
+        orders_reader = csv.DictReader(
+            file, fieldnames=["customer", "order", "day"]
+        )
+        for row in orders_reader:
+            order_list.append((row["customer"], row["order"], row["day"]))
+
+    return order_list
+
 class TrackOrders:
     # aqui deve expor a quantidade de estoque
     order_list = []
@@ -9,7 +38,9 @@ class TrackOrders:
        self.order_list.append([customer, order, day])
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        order_list = analyze_log("data/orders_1.csv")
+        orders = most_ordered_dish(order_list, customer)
+        return orders
 
     def get_never_ordered_per_customer(self, customer):
         pass
