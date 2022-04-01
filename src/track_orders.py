00cde514a4ca4b719926eceb410b41c8b20732dd
Eder Santos <3derms@gmail.com>
2022-04-01 14:02:06 -0300

    feat: cria a funçao add_new_order para registrar pedido na instancia

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,11 +1,12 @@
 class TrackOrders:
     # aqui deve expor a quantidade de estoque
+    order_list = []
     def __len__(self):
-        orders = []
-        return len(orders)
+        self.data = self.order_list
+        return len(self.data)
 
     def add_new_order(self, customer, order, day):
-        pass
+       self.order_list.append([customer, order, day])
 
     def get_most_ordered_dish_per_customer(self, customer):
         pass
